6a766948a2b8a0456fa8d6683c9608eb045bf4a8
Awais Mustafa <awesdon@gmail.com>
2019-01-17 21:40:38 +0500

    Updating file paths

--- cron.py
+++ cron.py
@@ -1,7 +1,7 @@
 from crontab import CronTab
 
 cron = CronTab('root')  
-job = cron.new(command='python ~/n.py')  
+job = cron.new(command='python ~/fabriccron/n.py')  
 job.minute.every(30)
 
 cron.write()
